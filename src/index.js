658fe4c1be22820da56510ddec40951ab9a1cb1b
hernantz <hernantz@gmail.com>
2018-02-20 16:13:15 -0300

    Added localStorage support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { loadState, saveState } from './storage'
 import React from 'react' 
 import { render } from 'react-dom'
 import todoApp  from './reducers'
 import App from './components/App'
 
 
-let store = createStore(todoApp, {todos: [{id: 0, text: 'Hola', completed: true}]},
+let store = createStore(todoApp, loadState(),
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
+
+store.subscribe(() => {
+    saveState(store.getState())
+})
 
 render(
     <Provider store={store}>
